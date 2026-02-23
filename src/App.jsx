28e0d91d47fc89262ee21a5506cb347f239ec648
Vicky Raj <vickyraj563065@gmail.com>
2026-02-23 20:25:41 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 // import "./App.css"
+
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
